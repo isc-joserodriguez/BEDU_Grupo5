fc332d62327f0dbd0f6dc35d6274685402508b73
Jose Rodriguez <isc.joserodriguez@gmail.com>
2021-04-16 18:51:47 -0600

    Added more components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import Layout from './hoc/Layout/Layout';
 import Login from './containers/Login/Login';
 import Admin from './containers/Admin/Admin';
 import Cliente from './containers/Cliente/Cliente';
 
-import './App.css';
-
 function App() {
 
   let routes = (
     <Switch>
-      {/* <Route path="/admin" render={(props) => <Auth {...props} />} /> */}
       <Route path="/" exact component={Login} />
       <Route path="/admin" exact component={Admin} />
       <Route path="/cliente" exact component={Cliente} />
-      {/* <Redirect to="/" /> */}
+      <Redirect to="/" />
     </Switch>
   );
 
