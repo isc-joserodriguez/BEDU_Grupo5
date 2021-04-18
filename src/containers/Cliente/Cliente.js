f8b37ebb617b085f36fffc3feae284440ec89711
Diego Sandoval <diegosv6910@gmail.com>
2021-04-18 10:05:50 -0500

    Agregando Clientes

--- src/containers/Cliente/Cliente.js
+++ src/containers/Cliente/Cliente.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import { React, Fragment, useState } from 'react'
+import FoodCards from './FoodCards'
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
 
-const Cliente = () => {
+function Cliente() {
+
+    const [category, setValue] = useState('recientes');
+
+    function menuAppers(e, foodCategory) {
+        e.preventDefault();
+        setValue(foodCategory);
+    }
+
+
     return (
-        <div>
-            Componente Cliente
-        </div>
-    )
+        <Fragment>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home" onClick={(e) => menuAppers(e, "recientes")}>Rest - aurante!</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#favoritos" onClick={(e) => menuAppers(e, "favoritos")}>Favoritos.</Nav.Link>
+                        <Nav.Link href="#recientes" onClick={(e) => menuAppers(e, "recientes")}>Recientes.</Nav.Link>
+                        <Nav.Link href="#promociones">Promociones.</Nav.Link>
+                        <Nav.Link href="#extras">Extras.</Nav.Link>
+                        <NavDropdown title="Categorias." id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Desayunos.</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Comidas.</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Cenas.</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Saludable.</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.5">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+            <FoodCards category = {category}/>
+        </Fragment>
+    );
 }
+
 
 export default Cliente
---
+++ src/containers/Cliente/FoodCards.js
@@ -0,0 +1,76 @@
+import { React } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import platilloFavorito from '../../assets/images/platillo_favorito.jpg';
+import platilloReciente from '../../assets/images/platillo_reciente.jpg';
+
+const arrayRecientes = ["Platiilo 1", "Nuevo Platillo Pruebalo", "Platillo 2", "Platillo Nuevo"]
+const arrayFavorites = ["Platillo 1", "Nuestro Platillo Favorito", "Platillo 2", "Un Clasico", "Platillo 3", "Muy Rico"]
+
+function DrawFoodCards(props) {
+    if(props.category === "recientes"){
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={platilloReciente} />
+            <Card.Body>
+                <Card.Title>{arrayRecientes[props.index]}</Card.Title>
+                <Card.Text>
+                    {arrayRecientes[props.index + 1]}
+                </Card.Text>
+                <Button variant="primary">Ordenar</Button>
+            </Card.Body>
+        </Card>
+    )
+    }else if(props.category === "favoritos"){
+        return (
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={platilloFavorito} />
+                <Card.Body>
+                    <Card.Title>{arrayFavorites[props.index]}</Card.Title>
+                    <Card.Text>
+                        {arrayFavorites[props.index + 1]}
+                    </Card.Text>
+                    <Button variant="primary">Ordenar</Button>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
+
+function RepeatFoodCards(props) {
+    if (props.category === "recientes") {
+        for (var index = 0; index < arrayRecientes.length; index++) {
+            return (
+                <Container>
+                    <Row className="mt-3">
+                        <Col><DrawFoodCards index={index} category = {props.category} /></Col>
+                        <Col><DrawFoodCards index={index + 2} category = {props.category} /></Col>
+                    </Row>
+                </Container>
+            )
+        }
+    } else if (props.category === "favoritos") {
+        for (index = 0; index < arrayFavorites.length; index++) {
+            return (
+                <Container>
+                    <Row className="mt-3">
+                        <Col><DrawFoodCards index={index} category = {props.category} /></Col>
+                        <Col><DrawFoodCards index={index + 2} category = {props.category} /></Col>
+                        <Col><DrawFoodCards index={index + 4} category = {props.category} /></Col>
+                    </Row>
+                </Container>
+            )
+        }
+    }
+}
+
+function foodCards(props) {
+    return(
+         <RepeatFoodCards category = {props.category}/>
+    )
+}
+
+export default foodCards
