50683a25b6183eb10ff6014a203e068317b89894
Jose Rodriguez <isc.joserodriguez@gmail.com>
2021-04-25 23:37:37 -0600

    [Modal] Added modal component

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './Modal.module.css';
+import Backdrop from '../Backdrop/Backdrop';
+
+const Modal = props => (
+    <>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}
+        >
+            {props.children}
+        </div>
+    </>
+)
+
+Modal.propTypes = {
+    show: PropTypes.bool.isRequired,
+    modalClosed: PropTypes.func.isRequired
+}
+
+export default Modal;
